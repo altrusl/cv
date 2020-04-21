b3e021053293b968a3e39f9b6fd017a8e976500e
Ruslan Makarov <ruslan.makarov@gmail.com>
2020-04-21 19:33:39 +0700

    scroll

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,6 @@
 let links = document.querySelectorAll("#header nav a");
 let articles = document.querySelectorAll("#main article");
+let scrollTos = document.querySelectorAll("#main .scrollTo");
 let linksArray = Array.prototype.slice.call(links);
 let main = document.querySelector("#main");
 // articles.forEach(article => {
@@ -8,10 +9,21 @@ let main = document.querySelector("#main");
 
 
 document.body.addEventListener("click", (event) => {
-    if(!event.target.closest('#main') && !linksArray.includes(event.target)) {
+    if (!event.target.closest('#main') && !linksArray.includes(event.target)) {
         console.log("outside");
         closeArticle();
-    }     
+    }
+});
+
+scrollTos.forEach(scrollTo => {
+    scrollTo.addEventListener("click", (event) => {
+        // const el = document.querySelector(scrollTo.dataset.target);
+        // scrollTo.closest(".article-content").scrollTop = el.offsetTop - 100;
+        scrollToElm(
+            scrollTo.closest(".article-content"),
+            document.querySelector(scrollTo.dataset.target),
+            0.8);
+    });
 });
 
 links.forEach(link => {
@@ -19,7 +31,7 @@ links.forEach(link => {
         console.log(event.target.getAttribute("href"));
         main.classList.remove("empty");
         articles.forEach(article => {
-            if (article == document.querySelector(event.target.getAttribute("href"))) {                
+            if (article == document.querySelector(event.target.getAttribute("href"))) {
                 article.style.display = "block";
                 setTimeout(() => {
                     article.classList.add("visible");
@@ -31,12 +43,12 @@ links.forEach(link => {
                 }, 300);
             }
         });
-        
+
     });
 });
 
 document.querySelector(".close-sign").addEventListener("click", (e) => {
-    closeArticle();      
+    closeArticle();
 });
 
 function closeArticle() {
@@ -48,11 +60,11 @@ function closeArticle() {
                 article.style.display = "none";
             }, 300);
         }
-    });  
+    });
 }
 
 
-document.addEventListener('keydown', function(e) {
+document.addEventListener('keydown', function (e) {
     let keyCode = e.keyCode;
     if (keyCode === 27) {
         closeArticle();
@@ -71,3 +83,50 @@ document.addEventListener('keydown', function(e) {
 //         }, 300);
 //     });
 // }
+
+
+/////////////
+
+function scrollToElm(container, elm, duration) {
+    var pos = getRelativePos(elm);
+    scrollTo(container, pos.top, duration); // duration in seconds
+}
+
+function getRelativePos(elm) {
+    var pPos = elm.parentNode.getBoundingClientRect(), // parent pos
+        cPos = elm.getBoundingClientRect(), // target pos
+        pos = {};
+
+    pos.top = cPos.top - pPos.top + elm.parentNode.scrollTop,
+        pos.right = cPos.right - pPos.right,
+        pos.bottom = cPos.bottom - pPos.bottom,
+        pos.left = cPos.left - pPos.left;
+
+    return pos;
+}
+
+function scrollTo(element, to, duration, onDone) {
+    var start = element.scrollTop,
+        change = to - start,
+        startTime = performance.now(),
+        val, now, elapsed, t;
+
+    function animateScroll() {
+        now = performance.now();
+        elapsed = (now - startTime) / 1000;
+        t = (elapsed / duration);
+
+        element.scrollTop = start + change * easeInOutQuad(t);
+
+        if (t < 1)
+            window.requestAnimationFrame(animateScroll);
+        else
+            onDone && onDone();
+    };
+
+    animateScroll();
+}
+
+function easeInOutQuad(t) {
+    return t < .5 ? 2 * t * t : -1 + (4 - 2 * t) * t
+};
