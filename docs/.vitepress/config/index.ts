b89e5b0b42a92b5fd6f3c1bc8a742fa247027ed9
Ruslan Makarov <ruslan.makarov@gmail.com>
2023-11-01 11:23:55 +0700

    refactor: v2

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base: "/cv/",
   lang: "en",
   title: "Ruslan Makarov",
   description: "Software Architect / Principal Engineer",
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,20 @@
 import { defineConfig } from "vite";
-import { SearchPlugin } from "vitepress-plugin-search";
+// import { SearchPlugin } from "vitepress-plugin-search";
 import vue from "@vitejs/plugin-vue";
 
 //default options
-var options = {
-  ...flexSearchIndexOptions,
-  previewLength: 62,
-  buttonLabel: "Search",
-  placeholder: "Search docs",
-  allow: [],
-  ignore: [],
-};
+// var options = {
+//   // ...flexSearchIndexOptions,
+//   previewLength: 62,
+//   buttonLabel: "Search",
+//   placeholder: "Search docs",
+//   allow: [],
+//   ignore: [],
+// };
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/cv/",
-  plugins: [vue(), SearchPlugin(options)],
+  // plugins: [vue(), SearchPlugin(options)],
+  plugins: [vue()],
 });
